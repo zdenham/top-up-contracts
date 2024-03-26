3fd7a3dd7a8847f51d0c5e94aba971986690d325
zdenham <zdenham1@gmail.com>
2024-03-26 09:17:52 -0700

    top up address

--- test/TopUp.ts
+++ test/TopUp.ts
@@ -2,118 +2,143 @@ import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 import { expect } from 'chai';
 import hre from 'hardhat';
 import { ethers } from 'hardhat';
-import { parseEther } from 'ethers';
+import { parseEther, Signer } from 'ethers';
+import { TopUp } from '../typechain-types';
+
+async function deployTopUpFixture() {
+  const [owner, otherAccount, receiver] = await ethers.getSigners();
+  const maxReceiverBalance = parseEther('1'); // Example max balance of 1 ether
+  const topUpAmount = parseEther('0.1'); // Example top-up amount of 0.1 ether
+
+  const ownerAddress = await owner.getAddress();
+  const otherAddress = await otherAccount.getAddress();
+  const receiverAddress = await receiver.getAddress();
+
+  const TopUp = await ethers.getContractFactory('TopUp');
+
+  const topUp = await TopUp.deploy(
+    owner.address,
+    maxReceiverBalance,
+    topUpAmount,
+    [receiverAddress]
+  );
+
+  // Sending some ether to the contract for top-up functionality
+  const initialContractBalance = parseEther('5');
+  const topUpAddress = await topUp.getAddress();
+  await owner.sendTransaction({
+    to: topUpAddress,
+    value: initialContractBalance,
+  });
+
+  return {
+    owner,
+    ownerAddress,
+    otherAccount,
+    receiver,
+    topUp,
+    topUpAmount,
+    initialContractBalance,
+    otherAddress,
+    receiverAddress,
+  };
+}
 
 describe('TopUp', function () {
-  async function deployTopUpFixture() {
-    const [owner, otherAccount, receiver] = await ethers.getSigners();
-    const maxReceiverBalance = parseEther('1'); // Example max balance of 1 ether
-    const topUpAmount = parseEther('0.1'); // Example top-up amount of 0.1 ether
-
-    const TopUp = await ethers.getContractFactory('TopUp');
-    const topUp = await TopUp.deploy(
-      owner.address,
-      maxReceiverBalance,
-      topUpAmount,
-      [receiver.address]
-    );
-
-    // Sending some ether to the contract for top-up functionality
-    const initialContractBalance = parseEther('5');
-    const topUpAddress = await topUp.getAddress();
-    await owner.sendTransaction({
-      to: topUpAddress,
-      value: initialContractBalance,
+  let topUp: TopUp;
+  let owner: Signer;
+  let otherAccount: Signer;
+  let receiver: Signer;
+  let topUpAmount: bigint;
+  let initialContractBalance: bigint;
+  let ownerAddress: string;
+  let otherAddress: string;
+  let receiverAddress: string;
+  context('with a topup contract deployed', async function () {
+    beforeEach(async function () {
+      ({
+        topUp,
+        owner,
+        otherAccount,
+        receiver,
+        topUpAmount,
+        initialContractBalance,
+        ownerAddress,
+        otherAddress,
+        receiverAddress,
+      } = await loadFixture(deployTopUpFixture));
     });
 
-    return {
-      owner,
-      otherAccount,
-      receiver,
-      topUp,
-      topUpAmount,
-      initialContractBalance,
-    };
-  }
+    it('should allow top up if the receiver address balance is less than the maxReceiverBalance', async function () {
+      // Ensuring the receiver's balance is less than maxReceiverBalance before top-up
+      expect(await ethers.provider.getBalance(receiverAddress)).to.be.lt(
+        topUpAmount
+      );
 
-  it('should allow top up if the receiver address balance is less than the maxReceiverBalance', async function () {
-    const { topUp, receiver, topUpAmount } = await loadFixture(
-      deployTopUpFixture
-    );
+      await expect(topUp.topUp(receiverAddress))
+        .to.emit(topUp, 'TopUpReceiver')
+        .withArgs(receiverAddress);
 
-    // Ensuring the receiver's balance is less than maxReceiverBalance before top-up
-    expect(await ethers.provider.getBalance(receiver.address)).to.be.lt(
-      topUpAmount
-    );
-
-    await expect(topUp.topUp(receiver.address))
-      .to.emit(topUp, 'TopUpReceiver')
-      .withArgs(receiver.address);
-
-    // The receiver's balance should increase by the topUpAmount
-    expect(await ethers.provider.getBalance(receiver.address)).to.equal(
-      topUpAmount
-    );
-  });
-
-  it('should emit a TopUp event when an address is topped up', async function () {
-    const { topUp, receiver } = await loadFixture(deployTopUpFixture);
-
-    await expect(topUp.topUp(receiver.address))
-      .to.emit(topUp, 'TopUpReceiver')
-      .withArgs(receiver.address);
-  });
-
-  it('should fail to top up if the receiver address balance is greater than the top up threshold', async function () {
-    const { topUp, receiver, owner } = await loadFixture(deployTopUpFixture);
-
-    // First, artificially increase the receiver's balance to be above the maxReceiverBalance
-    const highBalance = parseEther('2'); // 2 ether, for example
-    await owner.sendTransaction({
-      to: receiver.address,
-      value: highBalance,
+      // The receiver's balance should increase by the topUpAmount
+      expect(await ethers.provider.getBalance(receiverAddress)).to.equal(
+        topUpAmount
+      );
     });
 
-    // Attempting a top-up should fail
-    await expect(topUp.topUp(receiver.address)).to.be.revertedWithCustomError(
-      topUp,
-      'ReceiverBalanceTooHigh'
-    );
-  });
+    it('should emit a TopUp event when an address is topped up', async function () {
+      await expect(topUp.topUp(receiverAddress))
+        .to.emit(topUp, 'TopUpReceiver')
+        .withArgs(receiverAddress);
+    });
 
-  it('should fail to top up if the receiver was not passed in the constructor', async function () {
-    const { topUp, otherAccount } = await loadFixture(deployTopUpFixture);
+    it('should fail to top up if the receiver address balance is greater than the top up threshold', async function () {
+      // First, artificially increase the receiver's balance to be above the maxReceiverBalance
+      const highBalance = parseEther('2'); // 2 ether, for example
+      await owner.sendTransaction({
+        to: receiverAddress,
+        value: highBalance,
+      });
 
-    // Attempting to top-up an address not specified as a receiver should fail
-    await expect(
-      topUp.topUp(otherAccount.address)
-    ).to.be.revertedWithCustomError(topUp, 'NotReceiver');
-  });
+      // Attempting a top-up should fail
+      await expect(topUp.topUp(receiverAddress)).to.be.revertedWithCustomError(
+        topUp,
+        'ReceiverBalanceTooHigh'
+      );
+    });
 
-  it('should allow the withdrawAddress to withdraw', async function () {
-    const { topUp, owner, initialContractBalance } = await loadFixture(
-      deployTopUpFixture
-    );
+    it('should fail to top up if the receiver was not passed in the constructor', async function () {
+      // Attempting to top-up an address not specified as a receiver should fail
+      await expect(topUp.topUp(otherAddress)).to.be.revertedWithCustomError(
+        topUp,
+        'NotReceiver'
+      );
+    });
 
-    const initialOwnerBalance = await owner.;
+    it('should allow the withdrawAddress to withdraw', async function () {
+      const initialOwnerBalance = await ethers.provider.getBalance(
+        ownerAddress
+      );
 
-    // The withdraw function should successfully transfer all contract funds to the owner
-    const tx = await topUp.withdraw();
-    const receipt = await tx.wait();
-    const gasUsed = receipt.gasUsed.mul(receipt.effectiveGasPrice);
+      // The withdraw function should successfully transfer all contract funds to the owner
+      const tx = await topUp.withdraw();
+      const receipt = await tx.wait();
+      const gasUsed = receipt.gasUsed.mul(receipt.effectiveGasPrice);
 
-    // Owner's balance should increase by the initialContractBalance minus gas fees
-    expect(await owner.getBalance()).to.equal(
-      initialOwnerBalance.add(initialContractBalance).sub(gasUsed)
-    );
-  });
+      // Owner's balance should increase by the initialContractBalance minus gas fees
+      const balance = await ethers.provider.getBalance(ownerAddress);
+      expect(balance).to.equal(
+        initialOwnerBalance.add(initialContractBalance).sub(gasUsed)
+      );
+    });
 
-  it('should fail to withdraw if the caller is not the withdrawAddress', async function () {
-    const { topUp, otherAccount } = await loadFixture(deployTopUpFixture);
+    it('should fail to withdraw if the caller is not the withdrawAddress', async function () {
+      const otherTopUp = await topUp.connect(otherAccount);
 
-    // Attempting to withdraw with an account other than the owner should fail
-    await expect(topUp.connect(otherAccount).withdraw()).to.be.revertedWith(
-      'NotWithdrawAddress'
-    );
+      // Attempting to withdraw with an account other than the owner should fail
+      await expect(otherTopUp.withdraw()).to.be.revertedWithCustomError(
+        otherTopUp,
+        'NotWithdrawAddress'
+      );
+    });
   });
 });
