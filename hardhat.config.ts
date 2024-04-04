8add5d49dffb3a571664dac34d4cf432ec23bd23
zdenham <zdenham1@gmail.com>
2024-04-04 12:44:31 -0700

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,56 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
+
+const DEPLOYER_PK = process.env.JUICE_DEPLOYER_PK;
+
+// TODO - DRY -> resolution not working right now
+const rpcByNetwork = (network: string) => {
+  switch (network) {
+    default:
+      return 'https://api.avax.network/ext/bc/C/rpc';
+  }
+};
+
+const getChainConfig = (chain: string) => {
+  if (!DEPLOYER_PK) throw new Error('No deployer private key found');
+
+  switch (chain) {
+    default:
+      return {
+        url: rpcByNetwork(chain),
+        accounts: [DEPLOYER_PK],
+      };
+  }
+};
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.19', // Avalanche doesn't support >0.8.19
+  solidity: '0.8.19',
+  networks: {
+    avalanche: getChainConfig('avalanche'),
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    gasPrice: 21,
+  },
+  etherscan: {
+    apiKey: {
+      avalanche: 'blah',
+    },
+    customChains: [
+      {
+        network: 'avalanche',
+        chainId: 43114,
+        urls: {
+          apiURL:
+            'https://api.avascan.info/v2/network/mainnet/evm/43114/etherscan',
+          browserURL: 'https://avascan.info',
+        },
+      },
+    ],
+  },
 };
 
 export default config;
+
