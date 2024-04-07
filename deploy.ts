301e53e7b7754679b2e1d034acfaa71eb0bcc8da
zdenham <zdenham1@gmail.com>
2024-04-07 10:57:22 -0700

    fuji scan

--- deploy.ts
+++ deploy.ts
@@ -4,15 +4,25 @@ const withdrawAddress = '0x373567EFF052B296496862A285f347DbA0Aa8942';
 const maxTopUp = ethers.parseEther('5');
 const maxBalance = ethers.parseEther('6');
 
+const relayersProd = [
+  '0x926Ad961479Ed9C3a16f683c06AdA108dBB6B579',
+  '0xe0957D0B5927B563Cc98542308f1fc54C35A02Ea',
+  '0xD9595078875752f2A2FdcBd67839093347a05080',
+];
+
+const relayersStaging = ['0x5536F782b73780b8019770F09706d4fAa603f88d'];
+
 const deploy = async () => {
   console.log('ABOUT TO DEPLOY');
   const Factory = await ethers.getContractFactory('TopUp');
 
-  const contract = await Factory.deploy(withdrawAddress, maxBalance, maxTopUp, [
-    '0x926Ad961479Ed9C3a16f683c06AdA108dBB6B579',
-    '0xe0957D0B5927B563Cc98542308f1fc54C35A02Ea',
-    '0xD9595078875752f2A2FdcBd67839093347a05080',
-  ]);
+  const contract = await Factory.deploy(
+    withdrawAddress,
+    maxBalance,
+    maxTopUp,
+    // relayersProd
+    relayersStaging
+  );
 
   const address = await contract.getAddress();
   console.log('CONTRACT ADDRESS', address);
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,8 @@ const DEPLOYER_PK = process.env.JUICE_DEPLOYER_PK;
 // TODO - DRY -> resolution not working right now
 const rpcByNetwork = (network: string) => {
   switch (network) {
+    case 'fuji':
+      return 'https://api.avax-test.network/ext/bc/C/rpc';
     default:
       return 'https://api.avax.network/ext/bc/C/rpc';
   }
@@ -28,6 +30,7 @@ const config: HardhatUserConfig = {
   solidity: '0.8.19',
   networks: {
     avalanche: getChainConfig('avalanche'),
+    fuji: getChainConfig('fuji'),
   },
   gasReporter: {
     enabled: true,
@@ -42,6 +45,15 @@ const config: HardhatUserConfig = {
         chainId: 43114,
         urls: {
           apiURL: 'https://api.snowscan.xyz/api',
+          browserURL: 'https://avascan.info',
+        },
+      },
+      {
+        network: 'fuji',
+        chainId: 43113,
+        urls: {
+          apiURL:
+            'https://api.avascan.info/v2/network/testnet/evm/43113/etherscan',
           browserURL: 'https://avascan.info',
         },
       },
